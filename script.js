98b44a26f9575bbb452302d5f7fd61cbe4c6c4d8
Tadas <25231149+tsirvelis@users.noreply.github.com>
2024-02-07 19:56:04 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("as");
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice == 0) {
+        return "Rock";
+    } else if ( computerChoice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice());
