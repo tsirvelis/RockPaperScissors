ab80e6bbcd671c67dccdeafe0fdccdea6906a4e6
Tadas <25231149+tsirvelis@users.noreply.github.com>
2024-02-07 20:40:41 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ function getComputerChoice() {
 
     if (computerChoice == 0) {
         return "Rock";
-    } else if ( computerChoice == 1) {
+    } else if (computerChoice == 1) {
         return "Paper";
     } else {
         return "Scissors"
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    console.log("player :", playerSelection, ". Computer :", computerSelection);
+
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+
+console.log(playRound(playerSelection, computerSelection))
